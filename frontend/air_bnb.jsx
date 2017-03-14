fc4dd6fb2734ec6832fe8bb8b3ac2f6d87746db4
Harvey Mirijanyan <harvey.mirijanyan@gmail.com>
2017-03-14 15:43:37 -0700

    Add session action, reducer and store

--- frontend/air_bnb.jsx
+++ frontend/air_bnb.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as APIUtils from './util/session_api_util.js';
+import configureStore from './store/store.js';
 
 // document.addEventListener('DOMContentLoaded', () => {
 //   const root = document.getElementbyId('root');
@@ -10,6 +11,7 @@ import * as APIUtils from './util/session_api_util.js';
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
     ReactDOM.render(<h1>Welcome to AirBnb</h1>, root);
+    window.store = configureStore();
     window.login = APIUtils.login;
     window.signup = APIUtils.signup;
     window.logout = APIUtils.logout;
