1180b2b1ec9c33b5b7d89ad442b9605cafb3ee5e
Harvey Mirijanyan <harvey.mirijanyan@gmail.com>
2017-03-14 16:13:08 -0700

    Add app, root

--- frontend/air_bnb.jsx
+++ frontend/air_bnb.jsx
@@ -2,19 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as APIUtils from './util/session_api_util.js';
 import configureStore from './store/store.js';
-
-// document.addEventListener('DOMContentLoaded', () => {
-//   const root = document.getElementbyId('root');
-//   ReactDOM.render(<Root />, root);
-// });
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
-    const root = document.getElementById('root');
-    ReactDOM.render(<h1>Welcome to AirBnb</h1>, root);
-    window.store = configureStore();
-    window.login = APIUtils.login;
-    window.signup = APIUtils.signup;
-    window.logout = APIUtils.logout;
+  const store = configureStore();
+  const root = document.getElementById('root');
+  ReactDOM.render(<Root store={ store }/>, root);
+  window.login = APIUtils.login;
+  window.signup = APIUtils.signup;
+  window.logout = APIUtils.logout;
 });
+
 
 //{email: 'testsad', password: 'testing123', fname: 'f', lname: 'l'}
