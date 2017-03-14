b7eb9990de91b1abb33b422859df59df076d0782
Harvey Mirijanyan <harvey.mirijanyan@gmail.com>
2017-03-14 15:12:53 -0700

    Add ajax requests to Util

--- frontend/air_bnb.jsx
+++ frontend/air_bnb.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as APIUtils from './util/session_api_util.js';
 
 // document.addEventListener('DOMContentLoaded', () => {
 //   const root = document.getElementbyId('root');
@@ -9,4 +10,9 @@ import ReactDOM from 'react-dom';
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
     ReactDOM.render(<h1>Welcome to AirBnb</h1>, root);
+    window.login = APIUtils.login;
+    window.signup = APIUtils.signup;
+    window.logout = APIUtils.logout;
 });
+
+//{email: 'testsad', password: 'testing123', fname: 'f', lname: 'l'}
