f1134787f670b3e45cb020574e86fb02b8b58449
Harvey Mirijanyan <harvey.mirijanyan@gmail.com>
2017-03-16 15:47:58 -0700

    Add min-width to featured offices

--- frontend/components/office/office.jsx
+++ frontend/components/office/office.jsx
@@ -16,7 +16,10 @@ class Office extends React.Component {
         <h3 className='feature-title'>Featured Offices</h3>
         <ul className='image-list'>
         {this.props.offices.map(office => (
-          <li className='office-li'><img className='office-img' src={office.image_url}/></li>
+          <li className='office-li'>
+            <h1 className='office-price'>${office.price} {office.location}</h1>
+            <img className='office-img' src={office.image_url}/>
+          </li>
         ))}
       </ul>
       </div>
