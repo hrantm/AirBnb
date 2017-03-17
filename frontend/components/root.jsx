ba664b2541dd0a21380e5145a4d5aae13d646fa7
Harvey Mirijanyan <harvey.mirijanyan@gmail.com>
2017-03-17 15:50:48 -0700

    Add styling for search and fix features styling

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -5,6 +5,7 @@ import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import SessionFormContainer from './session_form/session_form_container.js';
 import OfficeContainer from './office/office_container';
 import OfficeDetailsContainer from './office/office_details_container';
+import SearchContainer from './search/search_container';
 
 const Root = ({ store }) => {
 
@@ -20,6 +21,7 @@ const Root = ({ store }) => {
     <Router history={ hashHistory }>
       <Route path="/" component={ App }>
         <IndexRoute component={ OfficeContainer } />
+        <Route path='/search' component={SearchContainer}/>
         <Route path="/profile" component={ SessionFormContainer } />
         <Route path='/offices'>
             <Route path='/offices/:office_id' component={ OfficeDetailsContainer }/>
