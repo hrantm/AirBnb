93d7e9de50f7c045072e741336aaa4a45fda288c
Harvey Mirijanyan <harvey.mirijanyan@gmail.com>
2017-03-23 08:52:22 -0700

    Add styling for review form

--- frontend/components/review/review_form.jsx
+++ frontend/components/review/review_form.jsx
@@ -5,22 +5,31 @@ import ReactStars from 'react-stars';
 class ReviewForm extends React.Component {
   constructor(props){
     super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
   }
 
   render(){
     console.log('Now its the form');
     console.log(this.props);
     return(
-      <form className='review-form'>
-        <h1>Add Review</h1>
-        <ReactStars
-          count={5}
-          size={24}
-          color2={'#ff7e82'} />
+      <form className='review-form' onSubmit={this.handleSubmit}>
+        <div className='review-form-header'>
+          <h1 className='review-form-title'>Add Review</h1>
+          <ReactStars
+            count={5}
+            size={24}
+            half={false}
+            color2={'#ff7e82'} />
+        </div>
         <textarea
           className='form-textarea'/>
         <input type='submit'
-          value='Submit Review'/>
+          value='Submit'
+          className='review-submit'/>
       </form>
     );
   }
